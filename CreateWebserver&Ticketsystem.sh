92e1b13c24a9ee905e7e5600da67dbf70b1d85ab
manuelsantoro06 <114143132+manuelsantoro06@users.noreply.github.com>
2022-12-15 10:49:55 +0100

    Erstellt ein Webserver und das Ticketsystem.
    Diese Datei erstellt ein Webserver und das Ticketsystem dazu.

---
+++ CreateWebserver&Ticketsystem.sh
@@ -0,0 +1,2 @@
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt install build-essential ruby-dev libxslt1-dev libmariadb-dev libxml2-dev zlib1g-dev imagemagick libmagickwand-dev curl gnupg2 bison libbison-dev libgdbm-dev libncurses-dev libncurses5-dev libreadline-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 -y
