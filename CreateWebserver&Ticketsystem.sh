1cc6d82e8a92c01324f7e5d5d617cb496f9f8680
manuelsantoro06 <114143132+manuelsantoro06@users.noreply.github.com>
2022-12-15 11:35:43 +0100

    installation Webserver und Installation von Redmine

--- CreateWebserver&Ticketsystem.sh
+++ CreateWebserver&Ticketsystem.sh
@@ -1,2 +1,6 @@
 sudo apt-get update && sudo apt-get dist-upgrade
 sudo apt install build-essential ruby-dev libxslt1-dev libmariadb-dev libxml2-dev zlib1g-dev imagemagick libmagickwand-dev curl gnupg2 bison libbison-dev libgdbm-dev libncurses-dev libncurses5-dev libreadline-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 -y
+sudo apt install apache2 libapache2-mod-passenger
+sudo systemctl enable --now apache2
+sudo apt install ruby-full
+sudo useradd -r -m -d /opt/redmine -s /usr/bin/bash redmine
