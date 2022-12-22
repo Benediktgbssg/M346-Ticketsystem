ec73f47fa55aa8b3a19c48daecb07bdc8fb986a5
Benediktgbssg <114584745+Benediktgbssg@users.noreply.github.com>
2022-12-22 23:37:19 +0100

    Update instance.sh
    Anpassung von Erstellung 2. Instanz und Kommentar anppasungen.

--- instance.sh
+++ instance.sh
@@ -9,20 +9,16 @@ aws ec2 create-security-group --group-name ticketsystem-group --description "EC2
 #Gruppe datenbank-group
 aws ec2 create-security-group --group-name datenbank-group --description "EC2-datenbank-group"
 
-#gruppe Rechte vergeben
+#gruppe ticketsystem-group Rechte vergeben
 aws ec2 authorize-security-group-ingress --group-name ticketsystem-group --protocol tcp --port 80 --cidr 0.0.0.0/0
 
-# installation von curl
-#sudo apt update
-#sudo install curl 
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#unzip awscliv2.zip
-#sudo ./aws/install
+#gruppe datenbank-groupe rechte vergeben 
+
 
 #instance-ticketsystem
-aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name $schluessel --security-groups ticketsystem-group --iam-instance-profile Name=LABInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test1}]'
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name $schluessel --security-groups ticketsystem-group --iam-instance-profile Name=LABInstanceProfile --user-data file://ticketsys.sh --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ticketsys}]'
 
 
 #instance-Database
-#aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name <schlüsselname> --security-groups <gruppenname> --iam-instance-profile Name=LABInstanceProfile --user-data file://xxxx --tag-specifications 'ResourceType=instance,Tags=[{Key=<name>,Value=<xxx>}]'
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name $schluessel --security-groups datenbank-group --iam-instance-profile Name=LABInstanceProfile --user-data file://datenbanksys.sh --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=datenbank}]'
 
