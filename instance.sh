1b7f4ba10a4d76a6678332a9ae8c6ba91ab1caf1
Benediktgbssg <114584745+Benediktgbssg@users.noreply.github.com>
2022-12-23 21:51:33 +0100

    instance test 1
    Das initial file hat ein fehler. Fehler gelöst. Der fehler war das die Formel zur einschreibung des ssh-schluessel falsch war, man darf nicht .pem haben.

--- instance.sh
+++ instance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-schluessel=aws-benedikt-cli.pem #links nach dem = den schlüssel Namen angeben
+schluessel=schluessel-test #links nach dem = den schlüssel Namen angeben ohne .pem
 #test von variabel
 #echo $schluessel
 
@@ -13,12 +13,10 @@ aws ec2 create-security-group --group-name ticketsystem-group --description "EC2
 aws ec2 authorize-security-group-ingress --group-name ticketsystem-group --protocol tcp --port 80 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name ticketsystem-group --protocol tcp --port 3306 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-name ticketsystem-group --protocol tcp --port 22 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-name ticketsystem-group --protocol tcp --port -1 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-name ticketsystem-group --protocol tcp --port 1 --cidr 0.0.0.0/0
 
 #instance-ticketsystem
-aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name $schluessel --security-groups ticketsystem-group --iam-instance-profile Name=LABInstanceProfile --user-data file://ticketsys.sh --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ticketsys}]'
-
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name $schluessel --security-groups ticketsystem-group --iam-instance-profile Name=LabInstanceProfile --user-data file://AllesInstallierenAufEinemServer.sh --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=ticketsys}]'
 
 #instance-Database
 aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name $schluessel --security-groups ticketsystem-group --iam-instance-profile Name=LABInstanceProfile --user-data file://datenbanksys.sh --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=datenbank}]'
-
